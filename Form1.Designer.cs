f92270d330770b5d4171c0e234dd9b4c04d9af58
Sergio Tomio <sergio-2343@hotmail.com>
2017-05-09 22:15:20 -0300

    AAAAAA

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace PiKanban
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstIssues = new System.Windows.Forms.ListBox();
+            this.lstBP = new System.Windows.Forms.ListBox();
+            this.lstMP = new System.Windows.Forms.ListBox();
+            this.lstAP = new System.Windows.Forms.ListBox();
+            this.lstAM = new System.Windows.Forms.ListBox();
+            this.lstMM = new System.Windows.Forms.ListBox();
+            this.lstBM = new System.Windows.Forms.ListBox();
+            this.lstAG = new System.Windows.Forms.ListBox();
+            this.lstMG = new System.Windows.Forms.ListBox();
+            this.lstBG = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstIssues
+            // 
+            this.lstIssues.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lstIssues.FormattingEnabled = true;
+            this.lstIssues.Location = new System.Drawing.Point(0, 0);
+            this.lstIssues.Name = "lstIssues";
+            this.lstIssues.Size = new System.Drawing.Size(151, 599);
+            this.lstIssues.TabIndex = 0;
+            // 
+            // lstBP
+            // 
+            this.lstBP.FormattingEnabled = true;
+            this.lstBP.Location = new System.Drawing.Point(100, 77);
+            this.lstBP.Name = "lstBP";
+            this.lstBP.Size = new System.Drawing.Size(120, 95);
+            this.lstBP.TabIndex = 1;
+            // 
+            // lstMP
+            // 
+            this.lstMP.FormattingEnabled = true;
+            this.lstMP.Location = new System.Drawing.Point(323, 77);
+            this.lstMP.Name = "lstMP";
+            this.lstMP.Size = new System.Drawing.Size(120, 95);
+            this.lstMP.TabIndex = 2;
+            // 
+            // lstAP
+            // 
+            this.lstAP.FormattingEnabled = true;
+            this.lstAP.Location = new System.Drawing.Point(546, 77);
+            this.lstAP.Name = "lstAP";
+            this.lstAP.Size = new System.Drawing.Size(120, 95);
+            this.lstAP.TabIndex = 3;
+            // 
+            // lstAM
+            // 
+            this.lstAM.FormattingEnabled = true;
+            this.lstAM.Location = new System.Drawing.Point(546, 211);
+            this.lstAM.Name = "lstAM";
+            this.lstAM.Size = new System.Drawing.Size(120, 95);
+            this.lstAM.TabIndex = 6;
+            // 
+            // lstMM
+            // 
+            this.lstMM.FormattingEnabled = true;
+            this.lstMM.Location = new System.Drawing.Point(323, 211);
+            this.lstMM.Name = "lstMM";
+            this.lstMM.Size = new System.Drawing.Size(120, 95);
+            this.lstMM.TabIndex = 5;
+            // 
+            // lstBM
+            // 
+            this.lstBM.FormattingEnabled = true;
+            this.lstBM.Location = new System.Drawing.Point(100, 211);
+            this.lstBM.Name = "lstBM";
+            this.lstBM.Size = new System.Drawing.Size(120, 95);
+            this.lstBM.TabIndex = 4;
+            // 
+            // lstAG
+            // 
+            this.lstAG.FormattingEnabled = true;
+            this.lstAG.Location = new System.Drawing.Point(546, 347);
+            this.lstAG.Name = "lstAG";
+            this.lstAG.Size = new System.Drawing.Size(120, 95);
+            this.lstAG.TabIndex = 9;
+            // 
+            // lstMG
+            // 
+            this.lstMG.FormattingEnabled = true;
+            this.lstMG.Location = new System.Drawing.Point(323, 347);
+            this.lstMG.Name = "lstMG";
+            this.lstMG.Size = new System.Drawing.Size(120, 95);
+            this.lstMG.TabIndex = 8;
+            // 
+            // lstBG
+            // 
+            this.lstBG.FormattingEnabled = true;
+            this.lstBG.Location = new System.Drawing.Point(100, 347);
+            this.lstBG.Name = "lstBG";
+            this.lstBG.Size = new System.Drawing.Size(120, 95);
+            this.lstBG.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "P";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "M";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "G";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(141, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(366, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "M";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(592, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "A";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(151, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(751, 599);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lstAP);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.lstBP);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.lstMP);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.lstBM);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.lstMM);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.lstAM);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lstBG);
+            this.tabPage1.Controls.Add(this.lstAG);
+            this.tabPage1.Controls.Add(this.lstMG);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(743, 573);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tamanho/Conhecimento";
+            this.tabPage1.ToolTipText = "Tamanho/Conhecimento";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "SP1";
+            this.tabPage2.ToolTipText = "SP1";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(192, 74);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "SP2";
+            this.tabPage3.ToolTipText = "SP2";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(192, 74);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "SP3";
+            this.tabPage4.ToolTipText = "SP4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(192, 74);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "SP4";
+            this.tabPage5.ToolTipText = "SP5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(192, 74);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "SP5";
+            this.tabPage6.ToolTipText = "SP6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(192, 74);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "SP6";
+            this.tabPage7.ToolTipText = "SP6";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 599);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.lstIssues);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstIssues;
+        private System.Windows.Forms.ListBox lstBP;
+        private System.Windows.Forms.ListBox lstMP;
+        private System.Windows.Forms.ListBox lstAP;
+        private System.Windows.Forms.ListBox lstAM;
+        private System.Windows.Forms.ListBox lstMM;
+        private System.Windows.Forms.ListBox lstBM;
+        private System.Windows.Forms.ListBox lstAG;
+        private System.Windows.Forms.ListBox lstMG;
+        private System.Windows.Forms.ListBox lstBG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using AnotherJiraRestClient;
+using AnotherJiraRestClient.JiraModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PiKanban
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //var jira = new JiraClient(new JiraAccount() { User = "sergio.tomio", Password = "456123@Capivara", ServerUrl = "http://jira.senior.com.br" });
+            //var issuesProjeto = jira.GetIssuesByJql("project = SDE AND type in (Story, Bug) AND status != Done ORDER BY Rank ASC", 0, Int32.MaxValue, new[] { "key", "summary" });
+
+            //var issuesBacklog = issuesProjeto.issues
+            //    .Where(issue => issue.fields.customfield_10004 == null ||
+            //                    !issue.fields.customfield_10004.Any(sprint => sprint.Contains("completeDate=<null>")));
+
+            var issuesBacklog = new List<Issue>()
+            {
+                new Issue() { key = "SDE-123", fields = new Fields() { summary = "Teste" } },
+                new Issue() { key = "SDE-321", fields = new Fields() { summary = "Teste2" } },
+                new Issue() { key = "SDE-456", fields = new Fields() { summary = "Teste3" } },
+            };
+
+            foreach (var issue in issuesBacklog)
+            {
+                lstIssues.Items.Add(issue.key + " - " + issue.fields.summary);
+            }
+
+            this.ResizeEnd += Resize;
+
+            Resize(null, null);
+        }
+
+        private void Resize(object sender, EventArgs e)
+        {
+            var margem = 10;
+
+            var topo1 = PercentualTopoTab(0.25f) + margem;
+            var topo2 = PercentualTopoTab(0.50f) + margem;
+            var topo3 = PercentualTopoTab(0.75f) + margem;
+
+            var esquerda1 = PercentualDireitaTab(0.25f) + margem;
+            var esquerda2 = PercentualDireitaTab(0.50f) + margem;
+            var esquerda3 = PercentualDireitaTab(0.75f) + margem;
+
+            label1.Location = new Point(tabPage1.Left + 10, topo1);
+            label2.Location = new Point(tabPage1.Left + 10, topo2);
+            label3.Location = new Point(tabPage1.Left + 10, topo3);
+            
+            label4.Location = new Point(esquerda1, tabPage1.Top + 10);
+            label5.Location = new Point(esquerda2, tabPage1.Top + 10);
+            label6.Location = new Point(esquerda3, tabPage1.Top + 10);
+
+            var largura = esquerda2 - esquerda1;
+            var altura = topo2 - topo1;
+
+            var metadeLargura = largura / 2;
+            var metadeAltura = altura / 2;
+
+            var tamanho = new Size(largura, altura);
+
+            lstBP.Location = new Point(esquerda1 - metadeLargura, topo1 - metadeAltura);
+            lstBP.Size = tamanho;
+            lstMP.Location = new Point(esquerda2 - metadeLargura, topo1 - metadeAltura);
+            lstMP.Size = tamanho;
+            lstAP.Location = new Point(esquerda3 - metadeLargura, topo1 - metadeAltura);
+            lstAP.Size = tamanho;
+
+            lstBM.Location = new Point(esquerda1 - metadeLargura, topo2 - metadeAltura);
+            lstBM.Size = tamanho;
+            lstMM.Location = new Point(esquerda2 - metadeLargura, topo2 - metadeAltura);
+            lstMM.Size = tamanho;
+            lstAM.Location = new Point(esquerda3 - metadeLargura, topo2 - metadeAltura);
+            lstAM.Size = tamanho;
+
+            lstBG.Location = new Point(esquerda1 - metadeLargura, topo3 - metadeAltura);
+            lstBG.Size = tamanho;
+            lstMG.Location = new Point(esquerda2 - metadeLargura, topo3 - metadeAltura);
+            lstMG.Size = tamanho;
+            lstAG.Location = new Point(esquerda3 - metadeLargura, topo3 - metadeAltura);
+            lstAG.Size = tamanho;
+        }
+
+        private int PercentualDireitaTab(float percentual)
+        {
+            return (int)((tabPage1.Right - tabPage1.Left) * percentual);
+        }
+
+        private int PercentualTopoTab(float percentual)
+        {
+            return (int)((tabPage1.Bottom - tabPage1.Top) * percentual);
+        }
+    }
+}
